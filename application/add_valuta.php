ac3ef3bd4ae3569ed46064984e181094423be121
Sara <123>
2016-01-15 21:27:07 +0100

    add_valuta

---
+++ application/add_valuta.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sara
+ * Date: 15/01/2016
+ * Time: 17:12
+ */
+
+
+$nome_valuta=$_POST['nome_valuta'];
+$simbolo_valuta=$_POST['simbolo_valuta'];
+
+
+
+print($nome_valuta." ".$simbolo_valuta);
