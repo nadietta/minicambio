1a569565f14c3b8b60c518ff16badd30d6e6c1e2
Sara <123>
2016-01-15 13:36:25 +0100

    Setup

--- application/menu.php
+++ application/menu.php
@@ -6,15 +6,17 @@
  * Time: 17:14
  */
 
- $nome_pagina = basename($_SERVER['PHP_SELF']); ?>
+ $nome_pagina = basename($_SERVER['HTTP_REFERER']);
 
+?>
+<link rel="icon" href="../img/biglietto_visita.PNG" type="image/gif" />
 <div class="pure-menu pure-menu-horizontal">
     <a href="#" class="pure-menu-heading">miniCAMBIO S.a.g.l.</a>
     <ul class="pure-menu-list">
-        <li class="pure-menu-item <?php if($nome_pagina == "index.html"){ print "pure-menu-selected";}?>><a href="#" class="pure-menu-link">Home</a></li>
+        <li class="pure-menu-item <?php if($nome_pagina == "index.html"){ print "pure-menu-selected";}?>"> <a href="index.html" class="pure-menu-link">Home</a></li>
         <li class="pure-menu-item"><a href="#" class="pure-menu-link">Nuova Operazione</a></li>
         <li class="pure-menu-item"><a href="#" class="pure-menu-link">Lista Operazioni</a></li>
         <li class="pure-menu-item"><a href="#" class="pure-menu-link">Report</a></li>
-        <li class="pure-menu-item"><a href="#" class="pure-menu-link">Setup</a></li>
+        <li class="pure-menu-item <?php if($nome_pagina == "setup.html"){ print "pure-menu-selected";}?>"><a href="setup.html" class="pure-menu-link">Setup</a></li>
     </ul>
 </div>
