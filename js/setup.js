ee89bf70617280d3013def4b6527b831abada7d8
Sara <123>
2016-01-17 12:17:47 +0100

    add_valuta

--- js/setup.js
+++ js/setup.js
@@ -54,6 +54,7 @@ $(document).ready(function() {
             success: function(data)
             {
                 var myresponse = $.parseJSON(data);
+
                 alert(myresponse);
 
             },
