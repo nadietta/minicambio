4f7cab52f3be49d6d15f5662acfc3155bd6c6d59
Sara <123>
2016-01-17 15:40:36 +0100

    lista valute + css img_icon

--- js/setup.js
+++ js/setup.js
@@ -62,10 +62,10 @@ $(document).ready(function() {
                 var myresponse = $.parseJSON(data);
                 if (myresponse.length>0) {
 
-                    table = " Lista Valute <table  class='pure-table'> " +
+                    table = "<legend> Lista Valute </legend> <table  class='pure-table'> " +
                         "<thead> <tr> <th>Numero</th> <th>Nome Valuta</th> <th>Simbolo Valuta</th> <th>Modifica</th> <th>Cancella</th>" +
-                        "</tr> </thead> </table>";
-                    for (var i = 0; i <= myresponse.length; i++) {
+                        "</tr> </thead> ";
+                    for (var i = 0; i <myresponse.length; i++) {
                         id = myresponse[i].id;
                         nome_valuta = myresponse[i].nome_valuta;
                         simbolo_valuta = myresponse[i].simbolo_valuta;
@@ -74,15 +74,16 @@ $(document).ready(function() {
                             "<td id='id_valuta" + id + "'>" + id + "</td>" +
                             "<td id='nome_valuta" + id + "'>" + nome_valuta + "</td>" +
                             "<td id='simbolo_valuta" + id + "'>" + simbolo_valuta + "</td>" +
-                            "<td align='center'><img src='../img/edit_icon.png' height='42' width='42'></td>" +
-                            "<td align='center'><img src='../img/delete_icon.png' height='42' width='42'></td>" +
+                            "<td align='center'><img class='img_icon' src='../img/edit_icon.png'></td>" +
+                            "<td align='center'><img class='img_icon' src='../img/delete_icon.png'></td>" +
                             "</tr>";
-                        alert(tr_table);
+
 
                     }
 
                     table +="</table>"
-                        $('#lista_valute_table').html(tr_table);
+
+                        $('#lista_valute_table').html(table);
                 }
                 else{
                     msg += "<br>Nessuna Valuta Presente.";
