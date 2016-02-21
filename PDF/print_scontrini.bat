a8751c7ac0b9cc8c4b135a5a341713fdbb9cc2bb
Sara <123>
2016-02-21 13:42:44 +0100

    print

--- PDF/print_scontrini.bat
+++
@@ -1,3 +0,0 @@
-@echo off 
-..\PDF\wkhtmltopdf\bin\wkhtmltopdf.exe  --page-size A5   ../PDF/Operazione1.html   ..\PDF\pdf_generate\scontrino1.pdf 
-..\PDF\wkhtmltopdf\bin\wkhtmltopdf.exe  --page-size A5   ../PDF/Operazione2.html   ..\PDF\pdf_generate\scontrino2.pdf 
